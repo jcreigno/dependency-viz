ea252845520609d0692ca6030df4b75f9af1c09e
jerome creignou <jerome.creignou@gmail.com>
2012-06-11 00:06:18 +0200

    add version autocomplete

--- src/main/webapp/js/raw.js
+++ src/main/webapp/js/raw.js
@@ -95,6 +95,56 @@
             });
             this.get('',function(){});
         }).run();
+
+        //setup typehead : thanks @geuis
+    var autocomplete = $('#version').typeahead({source:[],items:15}).on('keyup', function(ev){
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            //filter out up/down, tab, enter, and escape keys
+            if( $.inArray(ev.keyCode,[40,38,9,13,27]) === -1 ){
+                var self = $(this);
+                //set typeahead source to empty
+                self.data('typeahead').source = [];
+                //active used so we aren't triggering duplicate keyup events
+                if( !self.data('active') && self.val().length > 0){
+
+                    self.data('active', true);
+
+                    //Do data request. Insert your own API logic here.
+                    $.getJSON('api/tree/'+model.groupId()+'/'+model.artifactId(),{
+                        q: $(this).val()
+                    }, function(data) {
+
+                        //set this to true when your callback executes
+                        self.data('active',true);
+
+                        //Filter out your own parameters. Populate them into an array, since this is what typeahead's source requires
+                        //var arr = [],
+                        //    i=data.results.length;
+                        //while(i--){
+                        //    arr[i] = data.results[i].text
+                        //}
+
+                        //set your results into the typehead's source 
+                        self.data('typeahead').source = data;
+                        self.data('typeahead').item = function(el){
+                            return '<li><a href="'+model.groupId()+'/'+model.artifactId()+'/'+el+'"></a></li>';
+                        };
+
+                        //trigger keyup on the typeahead to make it search
+                        self.trigger('keyup');
+
+                    }).complete(function(){
+                        //All done, set to false to prepare for the next remote query.
+                        self.data('active', false);
+                    });
+
+                }
+            }
+        });
+
+
     });
 
 })(this.jQuery,this.ko);
